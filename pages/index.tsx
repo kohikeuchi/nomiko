dab446f351d28879bc52bf823adf33db989cf358
Koh <koh.ikeuchi@gmail.com>
2023-04-27 01:48:15 +0900

    リンクを追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,18 @@
 import { Inter } from "next/font/google";
 import { useRouter } from "next/router";
+import Link from "next/link";
 
 export default function Home() {
   const router = useRouter();
-  // const handleClick = (e) => {
-  //   e.preventDefault();
-  //   router.push(href);
-  // };
+
+  const handleClick = () => {
+    router.push("create-events");
+  };
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="title">
+        Read <Link href="/create-events">this page!</Link>
+      </h1>
+    </main>
   );
 }
